fa688c3f33534b666f06c6777c6cebd1a07af01f
Madhav Paudel <poudelmadhav143@gmail.com>
2017-03-21 10:51:33 +0545

    deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 	before_action :authenticate_user!, only: :create
-	
+
 	def create
 	  @post = Post.find(params[:post_id])
 	  @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
