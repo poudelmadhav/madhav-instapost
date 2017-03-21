14dbf9515eba42f8d2a5b157809d1011908a53c3
Madhav Paudel <poudelmadhav143@gmail.com>
2017-03-21 08:11:51 +0545

    displaying post added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create]
+	def index
+	  @posts = Post.all.order('created_at DESC')
+	end
+	
 	def new
 	  @post = Post.new
 	end
