374a99acec71a6877efd8544c704161f4d449fe1
Madhav Paudel <poudelmadhav143@gmail.com>
2017-03-21 10:01:10 +0545

    posting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+	before_action :authenticate_user!, only: :create
+	
+	def create
+	  @post = Post.find(params[:post_id])
+	  @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
+	  if @comment.valid?
+	    redirect_to root_path
+	  else
+	    flash[:alert] = "Invalid attributes."
+	    redirect_to root_path
+	  end
+	end
+
+	private 
+
+	def comment_params
+	  params.require(:comment).permit(:text, :post_id)
+	end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
 	belongs_to :user
 
 	belongs_to :posts
+
+	validates :text, :user_id, :post_id, presence: true
 end
