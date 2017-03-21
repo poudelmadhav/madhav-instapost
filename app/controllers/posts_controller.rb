568da22a06bdc5dfaf5d374b117587b5f0adfcc7
Madhav Paudel <poudelmadhav143@gmail.com>
2017-03-21 08:54:13 +0545

    filters added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create]
+	before_action :is_owner?, only: [:edit, :update]
+
 	def index
 	  @posts = Post.all.order('created_at DESC')
 	end
-	
+
 	def new
 	  @post = Post.new
 	end
@@ -17,7 +19,25 @@ class PostsController < ApplicationController
 	  end
 	end
 
+	def edit
+	  @post = Post.find(params[:id])
+	end
+
+	def update
+	  @post = Post.find(params[:id])
+	  @post.update(post_params)
+	  if @post.valid?
+	    redirect_to root_path
+	  else
+	    render :edit, status: :unprocessable_entity
+	  end
+	end
+
 	private
+
+	def is_owner?
+	  redirect_to root_path if Post.find(params[:id]).user != current_user
+	end
 
 	def post_params
 	  params.require(:post).permit(:user_id, :photo, :description)
