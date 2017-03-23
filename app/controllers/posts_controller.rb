f9007e81c561b3877593fa7842f38a2b9e19a5de
Madhav Paudel <poudelmadhav143@gmail.com>
2017-03-23 17:56:40 +0545

    pagination added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 	before_action :is_owner?, only: [:edit, :update, :destroy]
 
 	def index
-	  @posts = Post.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 2)
+	  @posts = Post.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 10)
 	end
 
 	def new
