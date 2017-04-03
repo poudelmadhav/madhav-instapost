b7513b9931696341d651a296eaf40a5d77c6a3a0
Madhav Paudel <poudelmadhav143@gmail.com>
2017-04-03 17:37:25 +0545

    tried to add cloudinary

--- Gemfile
+++ Gemfile
@@ -36,8 +36,6 @@ gem 'devise'
 
 # Use carrierwave for photo uploading feature
 gem 'carrierwave'
-gem 'mini_magick'
-gem 'fog'
 
 # Use simple form
 gem 'simple_form'
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,6 +4,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
+  # include Cloudinary::CarrierWave
+
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
