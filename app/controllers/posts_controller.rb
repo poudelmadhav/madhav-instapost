6e9624185d79a87f2321d913ab724dd972d0c269
Madhav Paudel <poudelmadhav143@gmail.com>
2017-04-04 17:37:09 +0545

    user authentication removed to delete the unknown posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
-	before_action :is_owner?, only: [:edit, :update, :destroy]
+	
 
 	def index
 	  @posts = Post.all.order('created_at DESC').includes(:user, comments: :user).paginate(:page => params[:page], :per_page => 10)
