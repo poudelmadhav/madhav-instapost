2f7fb3a2e7207737e84fd693ece833a4976f6b02
Madhav Paudel <poudelmadhav143@gmail.com>
2017-04-20 17:52:27 +0545

    favicon added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416031352) do
+ActiveRecord::Schema.define(version: 20170420110917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,6 @@ ActiveRecord::Schema.define(version: 20170416031352) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
   end
@@ -54,14 +52,6 @@ ActiveRecord::Schema.define(version: 20170416031352) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
     t.index ["user_id", "post_id"], name: "index_comments_on_user_id_and_post_id", using: :btree
-  end
-
-  create_table "ideas", force: :cascade do |t|
-    t.string   "author"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "priority"
   end
 
   create_table "posts", force: :cascade do |t|
