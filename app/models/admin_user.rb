87f914e55f7efdc132fe08485f59f05d32e0002a
Madhav Paudel <poudelmadhav143@gmail.com>
2017-04-20 16:55:09 +0545

    admin_user added to active admin

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,12 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :trackable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :trackable, :validatable
 end
