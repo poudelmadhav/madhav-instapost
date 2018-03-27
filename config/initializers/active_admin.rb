20c86b3df35013991ab0626cac03d72c8509e4f4
Madhav Paudel <poudelmadhav143@gmail.com>
2018-03-27 19:44:08 +0545

    active admin footer added

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -282,7 +282,7 @@ ActiveAdmin.setup do |config|
   # By default, the footer shows the current Active Admin version. You can
   # override the content of the footer here.
   #
-  # config.footer = 'my custom footer text'
+  config.footer = "Designed and Developed by Madhav Paudel."
 
   # == Sorting
   #
