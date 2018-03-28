99f5b6fa746e9198e2b918cc65bd1aee489365f1
Madhav Paudel <poudelmadhav143@gmail.com>
2018-03-28 17:18:17 +0545

    admin params added in user.rb

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :name, :email, :password, :password_confirmation, :photo
+permit_params :name, :email, :password, :password_confirmation, :photo, :admin
 #
 # or
 #
