935382ee53bf662a6c2cf37e9c99e9fd11620044
Madhav Paudel <poudelmadhav143@gmail.com>
2018-04-02 18:20:04 +0545

    comments order by created at desc

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
 
 	belongs_to :user
 	
-	has_many :comments, dependent: :destroy
+	has_many :comments, -> { order(created_at: :desc) }, dependent: :destroy
 
 	has_many :users, through: :comments
 
