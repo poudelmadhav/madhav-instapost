2237787ae66c5b1abcd7900fe07b82d209a05f81
Madhav Paudel <poudelmadhav143@gmail.com>
2018-04-03 09:57:27 +0545

    recent active users

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    section "Recent Users", :priority => 1 do
+    section "Recent Active Users", :priority => 1 do
         table_for User.order("current_sign_in_at DESC").limit(5) do
             column :id
             column "Name", :name do |user|
