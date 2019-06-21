e86cff125df87ed8422a026da693a02cd39d09e1
Madhav Paudel <poudelmadhav143@gmail.com>
2019-06-21 08:59:15 +0545

    domain added in mail

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -4,6 +4,7 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com'
+  :domain         => 'instapost.paudelmadhav.com.np',
+  :enable_starttls_auto => true
 }
 ActionMailer::Base.delivery_method = :smtp
