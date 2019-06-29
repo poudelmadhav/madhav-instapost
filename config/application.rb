1e18ca1dd22fd496da10839762a6f53ffe51fca2
Madhav Paudel <poudelmadhav143@gmail.com>
2019-06-29 14:41:56 +0545

    production environment of dev:true for errors in application for developmnet commented out

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ module Instapost
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.exception_handler = { dev: true }
+    # config.exception_handler = { dev: true }
   end
 end
