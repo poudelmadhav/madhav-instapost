af86fa88243e7b4e459979c5bd67482a41b00db3
Madhav Paudel <poudelmadhav143@gmail.com>
2025-11-30 18:03:22 +0545

    Commented unused code

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,11 @@
-ActionMailer::Base.smtp_settings = {
-  user_name: ENV.fetch('SMTP_USERNAME', nil),
-  password: ENV.fetch('SMTP_PASSWORD', nil),
-  domain: 'poudelmadhav.com.np',
-  address: 'smtp-relay.brevo.com',
-  port: 587,
-  authentication: :plain,
-  enable_starttls_auto: true
-}
+# ActionMailer::Base.smtp_settings = {
+#   user_name: ENV.fetch('SMTP_USERNAME', nil),
+#   password: ENV.fetch('SMTP_PASSWORD', nil),
+#   domain: 'poudelmadhav.com.np',
+#   address: 'smtp-relay.brevo.com',
+#   port: 587,
+#   authentication: :plain,
+#   enable_starttls_auto: true
+# }
 
-ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.delivery_method = :smtp
